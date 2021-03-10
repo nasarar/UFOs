16dfc6a7a50bf634d78f4c08c5f7bbb22dfc9b3f
unknown <nasara.ryan@gmail.com>
2021-03-10 16:41:32 -0500

    added button functionality on app.js to filter tableData

--- static/js/app.js
+++ static/js/app.js
@@ -9,11 +9,13 @@ function buildTable(data){
     // clears the table of pre-existing filters
     tbody.html("");
 
-    // function iterating through the data
+    // function iterating through each Object in the data
+    // adds a row and cells for each values in the row
     data.forEach((dataRow) => {
         let row = tbody.append('tr'); // adds row to the table body.. (tr) is HTML for table row
         
-        //adds each value from the object into a cell
+        // loops through each field in the dataRow and
+        //adds each value from the object into a table cell
         Object.values(dataRow).forEach((val)=> {
             let cell = row.append('td');
             cell.text(val);
@@ -23,3 +25,27 @@ function buildTable(data){
     });
 
 }
+
+// function to handling after an input is given
+function handleClick(){
+
+    // selects first 'datetime' id in the HTML script and grabs information to store into date variable 
+    let date = d3.select('#datetime').property('value'); 
+
+    // set to raw data to use as blank slate incase no filters are selected. All data wil be displayed
+    let filteredData = tableData;
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //calls the buildTable function and applies it on the filteredData
+    //if no filter is selected then original tableData will be displayed
+    buildTable(filteredData);
+
+};
+
+// listens if a button is clicked
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+//builds the table when web page loads
+buildTable(tableData);
